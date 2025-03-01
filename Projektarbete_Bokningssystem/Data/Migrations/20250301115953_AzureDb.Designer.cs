c95c59e05d027e514112aa8582540bbc266d000f
Filippannilsson <filippanilsson95@gmail.com>
2025-03-01 13:10:15 +0100

    Lagt till AzureDb

--- Projektarbete_Bokningssystem/Data/Migrations/20250301115953_AzureDb.Designer.cs
+++ Projektarbete_Bokningssystem/Data/Migrations/20250301115953_AzureDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Projektarbete_Bokningssystem.Data;
 
@@ -11,9 +12,11 @@ using Projektarbete_Bokningssystem.Data;
 namespace Projektarbete_Bokningssystem.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250301115953_AzureDb")]
+    partial class AzureDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
