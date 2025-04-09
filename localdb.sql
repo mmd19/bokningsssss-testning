21b7b411355f5a9e5449b418664b5fe8262c3f73
mmdnn <mmdnn@M>
2025-04-09 17:29:15 +0200

    Test

---
+++ localdb.sql
@@ -0,0 +1,12 @@
+﻿CREATE DATABASE S
+
+CREATE TABLE [dbo].[StudyRooms] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_StudyRooms] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+-- Lägg till studierum
+INSERT INTO StudyRooms (Name) VALUES ('Jupiter');
+INSERT INTO StudyRooms (Name) VALUES ('Telescope');
+INSERT INTO StudyRooms (Name) VALUES ('Science');
