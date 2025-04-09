4dc0d85ba907f2a8728295e8e5ff16ece5ef7cd4
mmdnn <mmdnn@M>
2025-04-10 01:57:46 +0200

    nn

--- Booking-Test/Enhet/Pages/CreateTest.cs
+++ Booking-Test/Enhet/Pages/CreateTest.cs
@@ -3,16 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Projektarbete_Bokningssystem.Data;
+using Projektarbete_Bokningssystem.Pages.Bookings;
 
 namespace Booking_Test.Enhet.Pages
 {
     public class CreateTest
     {
-        [Fact]
-        public void hej()
+         [Fact]
+        public async void createBooking()
         {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            using var context = new ApplicationDbContext(options);
+            context.Database.EnsureCreated(); // required for SQLite
+
+
+            var userManager = GetMockUserManager<IdentityUser>();
+            var createModel = new CreateModel(context, userManager.Object);
+            var testUser = new IdentityUser { Email = "test@example.com" };
+
+
+            var mockUserManager = GetMockUserManager<IdentityUser>();
+            mockUserManager.Setup(x => x.GetUserAsync(createModel.User))
+                           .ReturnsAsync(testUser);
+
+            Assert.Empty(context.Bookings);
+
+            await createModel.OnPostAsync();
+
+            Assert.Empty(context.Bookings);
+
 
         }
-        
+
+        public static Mock<UserManager<TUser>> GetMockUserManager<TUser>() where TUser : class
+        {
+            var store = new Mock<IUserStore<TUser>>();
+            var mgr = new Mock<UserManager<TUser>>(
+                store.Object,
+                null, null, null, null, null, null, null, null
+            );
+            return mgr;
+        }
+
     }
 }
