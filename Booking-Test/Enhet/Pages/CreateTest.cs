72013c80b220bcfd37981775e39308774ac15292
mmdnn <mmdnn@M>
2025-04-10 23:05:14 +0200

    mm

--- Booking-Test/Enhet/Pages/CreateTest.cs
+++ Booking-Test/Enhet/Pages/CreateTest.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Moq;
@@ -27,15 +31,15 @@ namespace Booking_Test.Enhet.Pages
             using var context = new ApplicationDbContext(options);
             context.Database.EnsureCreated(); // required for SQLite
 
+            var testUser = new IdentityUser { Email = "test@example.com" };
+
+            var mockUserManager = GetMockUserManager<IdentityUser>();
+            mockUserManager.Setup(um => um.GetUserAsync(It.IsAny<ClaimsPrincipal>()))
+                           .ReturnsAsync(testUser);
 
             var userManager = GetMockUserManager<IdentityUser>();
             var createModel = new CreateModel(context, userManager.Object);
-            var testUser = new IdentityUser { Email = "test@example.com" };
 
-
-            var mockUserManager = GetMockUserManager<IdentityUser>();
-            mockUserManager.Setup(x => x.GetUserAsync(createModel.User))
-                           .ReturnsAsync(testUser);
 
             Assert.Empty(context.Bookings);
 
